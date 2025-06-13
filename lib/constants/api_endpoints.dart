2ae65a844566d9cc4f8fff9b4fbccf3524bde058
Saqib Ali <saqibali.fd@gmail.com>
2025-06-13 16:06:30 +0500

    your data

--- lib/constants/api_endpoints.dart
+++ lib/constants/api_endpoints.dart
@@ -1,5 +1,5 @@
 class ApiEndpoints {
-  static const String apiKey = "AIzaSyCJurq_9PIOcUAnxy49APV0tYB9ro2N3IE";
+  static const String apiKey = "you api key";
   static const String baseUrl =
       "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=";
 }
